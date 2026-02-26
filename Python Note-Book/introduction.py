7a7c954c85e8053261590fd81185c0bdf6ea893d
Ahsab Masood <ahsabmsaood66@gmail.com>
2026-02-26 20:02:18 +0500

    Python Note-book

---
+++ Python Note-Book/introduction.py
@@ -0,0 +1,9 @@
+# First Python program
+print("Hello, World!")
+# Python version check
+import sys
+print(sys.version) # e.g. 3.11.0
+# Python as a calculator
+print(2 + 3) # 5
+print(10 / 3) # 3.3333...
+print(2 ** 10) # 1024
